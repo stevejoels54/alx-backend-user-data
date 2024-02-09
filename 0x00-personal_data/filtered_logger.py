410acadf97f04940780fdc9feb355d33d696746a
joelsteven <joelofelectronics@gmail.com>
2024-02-09 04:51:03 +0300

    Fix: PII_FIELDS tuple

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ from typing import List
 import logging
 
 
-PII_FIELDS = ("name", "email", "phone", "address", "credit_card")
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
