442179a00856f1ced0194a49e51ed5292b2f1513
joelsteven <joelofelectronics@gmail.com>
2024-02-09 03:58:29 +0300

    minor fixes

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,10 +8,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """
-     Returns a string with the specified fields obfuscated
-    """
-
+    """Returns string with the specified fields obfuscated"""
     for field in fields:
         message = re.sub(f'{field}=.*?{separator}',
                      f'{field}={redaction}{separator}', message)
