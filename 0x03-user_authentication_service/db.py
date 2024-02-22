e3f16c17493218820dcf59dabfd0ac04f721d281
joelsteven <joelofelectronics@gmail.com>
2024-02-22 05:19:01 +0300

    Fix imports

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import Base
 
