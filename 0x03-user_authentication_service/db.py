4eedf97b3261867be9a29b8cca4208062b431253
joelsteven <joelofelectronics@gmail.com>
2024-02-22 04:22:06 +0300

    Fix add user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,10 +30,8 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, user: User) -> None:
         """Adds new user to database
         """
-        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
-        return user
