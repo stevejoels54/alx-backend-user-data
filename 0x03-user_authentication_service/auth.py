cc3bd8268a32e4e12df01b938e102a912dbbcc3d
joelsteven <joelofelectronics@gmail.com>
2024-02-23 04:37:28 +0300

    Add Generate UUIDs

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@
 import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
 from db import DB
+import uuid
 
 from user import User
 
@@ -12,6 +13,12 @@ def _hash_password(password: str) -> str:
     """Hash password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generate UUID
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -53,10 +60,13 @@ class Auth:
         """
         Create session
         """
-        user = self._db.find_user_by(email=email)
-        session_id = _hash_password(email)
-        self._db.update_user(user.id, session_id=session_id)
-        return session_id
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except (NoResultFound, ValueError):
+            return None
 
     def get_user_from_session_id(self, session_id: str) -> str:
         """
@@ -99,7 +109,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = _hash_password(email)
+            reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
         except NoResultFound:
