bf779be9398895165c492893fb5dd6aaddd50c2e
joelsteven <joelofelectronics@gmail.com>
2024-02-23 05:33:17 +0300

    fix profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,6 +79,8 @@ def profile() -> Union[str, tuple]:
       - profile of user
     """
     session_id = request.cookies.get("session_id")
+    if not session_id:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
