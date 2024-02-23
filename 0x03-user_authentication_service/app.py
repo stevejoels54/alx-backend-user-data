a27015bca732416e489dcb1ac73e9d6dc3043d14
joelsteven <joelofelectronics@gmail.com>
2024-02-23 05:28:33 +0300

    Fix response

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Flask app
 """
-from flask import Flask, jsonify, request, abort, redirect, url_for
+from flask import Flask, jsonify, request, abort, redirect, url_for, Response
 from auth import Auth
 from typing import Union
 
@@ -69,11 +69,11 @@ def logout():
     if not user:
         abort(403)
     AUTH.destroy_session(user.id)
-    return redirect(url_for("home"))
+    return redirect("/")
 
 
 @app.route('/profile', methods=['GET'], strict_slashes=False)
-def profile() -> Union[str, tuple]:
+def profile() -> Response:
     """GET /profile
     Return:
       - profile of user
